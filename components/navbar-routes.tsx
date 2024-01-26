bbbf4b91930d2f5aa061f87c4495a78097edd16b
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-26 17:06:52 +0530

    add teacher mode routes

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,36 @@
-import { UserButton } from "@clerk/nextjs"
+"use client";
 
+import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 const NavBarRoutes = () => {
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isCoursePage = pathname?.includes("/courses");
+
   return (
-    <div className="ml-auto">
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isCoursePage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
       <UserButton afterSignOutUrl="/" />
     </div>
-  )
-}
+  );
+};
 
-export default NavBarRoutes
+export default NavBarRoutes;
