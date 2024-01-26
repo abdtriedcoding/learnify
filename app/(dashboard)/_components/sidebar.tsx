ccf0ad07ed8aef61749b96a320392a5045720660
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-26 15:54:50 +0530

    sidebar ui completed

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import SidebarRoutes from "./sidebar-routes";
+import Link from "next/link";
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="h-full w-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <Link href={"/"}>
+      <Image
+        className="mx-6 my-4"
+        height={130}
+        width={130}
+        alt="logo"
+        src="https://raw.githubusercontent.com/AntonioErdeljac/next13-lms-platform/f65539069d0d88dc6a31b125b2c81b474640b16f/public/logo.svg"
+      />
+      </Link>
+      <div className="flex flex-col w-full">
+        <SidebarRoutes />
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ const DashboardLayout = ({
 }) => {
   return ( 
     <div className="h-full flex">
-      <div className="hidden md:flex h-full w-56">
+      <div className="hidden md:flex h-full w-64">
         <Sidebar />
       </div>
       <main className="flex-1 h-full overflow-y-auto">
