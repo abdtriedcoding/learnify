5ce3144b57b89c3848322e3c3915115e71bf26c2
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-29 23:09:25 +0530

    add function to get all courses to display on ui

---
+++ app/actions/getCourses.ts
@@ -0,0 +1,71 @@
+import { Course } from "@prisma/client";
+
+import { db } from "@/lib/db";
+import { userProgress } from "./userProgress";
+
+type CourseWithProgress = Course & {
+  chapters: { id: string }[];
+  progress: number | null;
+};
+
+type GetCourses = {
+  userId: string;
+  title?: string;
+};
+
+export const getCourses = async ({
+  userId,
+  title,
+}: GetCourses): Promise<CourseWithProgress[]> => {
+  try {
+    const courses = await db.course.findMany({
+      where: {
+        isPublished: true,
+        title: {
+          contains: title,
+        },
+      },
+      include: {
+        chapters: {
+          where: {
+            isPublished: true,
+          },
+          select: {
+            id: true,
+          },
+        },
+        purchases: {
+          where: {
+            userId,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const coursesWithProgress: CourseWithProgress[] = await Promise.all(
+      courses.map(async (course) => {
+        if (course.purchases.length === 0) {
+          return {
+            ...course,
+            progress: null,
+          };
+        }
+
+        const progressPercentage = await userProgress(userId, course.id);
+
+        return {
+          ...course,
+          progress: progressPercentage,
+        };
+      })
+    );
+
+    return coursesWithProgress;
+  } catch (error) {
+    console.log("[GET_COURSES]", error);
+    return [];
+  }
+};
