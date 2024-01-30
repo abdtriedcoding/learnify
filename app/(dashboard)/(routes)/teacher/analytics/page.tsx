f13c8ff0f72f0442faaea18b654a111e505c4b77
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-31 00:28:52 +0530

    bar charts added in teacher analytics page

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,28 @@
-const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+import { getAnalytics } from "@/app/actions/getAnalytics";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import DataCard from "./_components/data-card";
+import Charts from "./_components/chart";
 
-export default Page
+const Page = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+  console.log(data);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+      <Charts data={data} />
+    </div>
+  );
+};
+
+export default Page;
