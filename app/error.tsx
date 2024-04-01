8dbe7cb6092c3fb62d6a43065cbf2ae52f69f29c
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-04-01 23:11:28 +0530

    fix error image

--- app/error.tsx
+++ app/error.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 const Error = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
-      <Image src="/error-dark.png" height="300" width="300" alt="Error" />
+      <Image src="/error.png" height="300" width="300" alt="Error" />
       <h2 className="text-xl font-medium">Something went wrong!</h2>
       <Button asChild>
         <Link href="/">Go back</Link>
