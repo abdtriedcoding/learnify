a733dcbc6ff0b1ea33376026026b69ad4c049bff
abdtriedcoding <siddabdullahofficial@gmail.com>
2024-05-30 15:43:50 +0530

    add: error and not-found page

--- app/error.tsx
+++ app/error.tsx
@@ -1,17 +1,32 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
-
+import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 
-const Error = () => {
+const Error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
   return (
-    <div className="h-full flex flex-col items-center justify-center space-y-4">
+    <div className="min-h-screen flex flex-col items-center justify-center space-y-4">
       <Image src="/error.png" height="300" width="300" alt="Error" />
       <h2 className="text-xl font-medium">Something went wrong!</h2>
-      <Button asChild>
-        <Link href="/">Go back</Link>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
       </Button>
     </div>
   );
